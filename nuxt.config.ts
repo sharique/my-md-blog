4f446b5836cf5265df12c68fd68f9e951e840a59
Sharique A Farooqui <safknw@gmail.com>
2026-04-10 10:03:06 +0200

    Trying to fix error on vercel.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,10 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 
+  nitro: {
+    preset: "vercel-static",
+  },
+
   modules: [
     "@nuxt/content",
     "@nuxt/eslint",
